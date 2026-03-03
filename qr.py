599758ab90e144b3f9048606e741a6a4ca728a12
Max H. Koren <mhkoren@afs-enclave.com>
2026-03-03 18:29:54 -0500

    major overhaul, classes, version support, not sure how ecc blocks work

--- qr.py
+++ qr.py
@@ -4,30 +4,27 @@ import string
 from collections import deque
 from itertools import batched, chain, repeat
 
+from rsconst import *
+
 from qrcodegen import QrCode, QrSegment
 
 
-def verify_nayuki(message):
-	seg = QrSegment.make_bytes(message.encode('utf-8'))
-	qr = QrCode.encode_segments([seg], QrCode.Ecc.LOW, minversion=4, maxversion=4, mask=0, boostecl=False)
-	return tuple(bytes(1 if qr.get_module(j, i) else 0 for j in range(33)) for i in range(33))
+def verify(message, version, encoding):
+	if encoding == 'b':
+		seg = QrSegment.make_bytes(message.encode('utf-8'))
+	elif encoding == 'a':
+		seg = QrSegment.make_alphanumeric(message)
+	else:
+		raise ValueError(encoding)
+		
+	qr = QrCode.encode_segments([seg], QrCode.Ecc.LOW, minversion=version, maxversion=version, mask=0, boostecl=False)
+	size = qr.get_size()
+	matrix = Matrix(size)
+	for j in range(size):
+		for i in range(size):
+			matrix.pxl_on(i, j, qr.get_module(j, i))
 
-
-def verify_nayuki_alphanumeric(message):
-	seg = QrSegment.make_alphanumeric(message)
-	qr = QrCode.encode_segments([seg], QrCode.Ecc.LOW, minversion=4, maxversion=4, mask=0, boostecl=False)
-	return tuple(bytes(1 if qr.get_module(j, i) else 0 for j in range(33)) for i in range(33))
-
-
-
-GF256_LOG = bytes([0,0,1,25,2,50,26,198,3,223,51,238,27,104,199,75,4,100,224,14,52,141,239,129,28,193,105,248,200,8,76,113,5,138,101,47,225,36,15,33,53,147,142,218,240,18,130,69,29,181,194,125,106,39,249,185,201,154,9,120,77,228,114,166,6,191,139,98,102,221,48,253,226,152,37,179,16,145,34,136,54,208,148,206,143,150,219,189,241,210,19,92,131,56,70,64,30,66,182,163,195,72,126,110,107,58,40,84,250,133,186,61,202,94,155,159,10,21,121,43,78,212,229,172,115,243,167,87,7,112,192,247,140,128,99,13,103,74,222,237,49,197,254,24,227,165,153,119,38,184,180,124,17,68,146,217,35,32,137,46,55,63,209,91,149,188,207,205,144,135,151,178,220,252,190,97,242,86,211,171,20,42,93,158,132,60,57,83,71,109,65,162,31,45,67,216,183,123,164,118,196,23,73,236,127,12,111,246,108,161,59,82,41,157,85,170,251,96,134,177,187,204,62,90,203,89,95,176,156,169,160,81,11,245,22,235,122,117,44,215,79,174,213,233,230,231,173,232,116,214,244,234,168,80,88,175])
-
-GF256_EXP = bytes([1,2,4,8,16,32,64,128,29,58,116,232,205,135,19,38,76,152,45,90,180,117,234,201,143,3,6,12,24,48,96,192,157,39,78,156,37,74,148,53,106,212,181,119,238,193,159,35,70,140,5,10,20,40,80,160,93,186,105,210,185,111,222,161,95,190,97,194,153,47,94,188,101,202,137,15,30,60,120,240,253,231,211,187,107,214,177,127,254,225,223,163,91,182,113,226,217,175,67,134,17,34,68,136,13,26,52,104,208,189,103,206,129,31,62,124,248,237,199,147,59,118,236,197,151,51,102,204,133,23,46,92,184,109,218,169,79,158,33,66,132,21,42,84,168,77,154,41,82,164,85,170,73,146,57,114,228,213,183,115,230,209,191,99,198,145,63,126,252,229,215,179,123,246,241,255,227,219,171,75,150,49,98,196,149,55,110,220,165,87,174,65,130,25,50,100,200,141,7,14,28,56,112,224,221,167,83,166,81,162,89,178,121,242,249,239,195,155,43,86,172,69,138,9,18,36,72,144,61,122,244,245,247,243,251,235,203,139,11,22,44,88,176,125,250,233,207,131,27,54,108,216,173,71,142])
-
-RS_POLY_20 = bytes([152, 185, 240, 5, 111, 99, 6, 220, 112, 150, 69, 36, 187, 22, 228, 198, 121, 121, 165, 174])
-RS_POLY_20_LOG = bytes([17, 60, 79, 50, 61, 163, 26, 187, 202, 180, 221, 225, 83, 239, 156, 164, 212, 212, 188, 190])
-
-RS_POLY_7 = bytes([127, 122, 154, 164, 11, 68, 117])
+	return matrix
 
 
 ALPHANUMERIC = string.digits + string.ascii_uppercase + ' $%*+-./:'
@@ -36,104 +33,153 @@ AN_TABLE = { a: i for i, a in enumerate(ALPHANUMERIC) }
 FORMAT_L0 = 0b111011111000100
 
 
-matrix = tuple(bytearray(33) for _ in range(33))
+class Matrix:
 
+	def __init__(self, size):
+		self.matrix = tuple(bytearray(size) for _ in range(size))
+	
+	@property
+	def size(self):
+		return len(self.matrix)
 
-def pxl_on(i, j, state=1):
-	if not (0 <= i < 33) or not (0 <= j < 33):
-		raise ValueError(i, j)
-	matrix[i][j] = state
+	def pxl_on(self, i, j, state=1):
+		if not (0 <= i < self.size) or not (0 <= j < self.size):
+			raise ValueError(i, j)
+		self.matrix[i][j] = state
 
+	def disp(self):
+		print('▒' * (self.size + 4))
+		for row1, row2 in batched(chain(self.matrix, [(0 for _ in range(self.size))]), 2):
+			print('▒▒', end='')
+			for px1, px2 in zip(row1, row2):
+				print(' ▀▄█'[~(px1 | (px2 << 1))], end='')
+			print('▒▒')
+		print('▒' * (self.size + 4))
 
-def disp_graph(graph=matrix):
-	print('▒' * 37)
-	for row1, row2 in batched(chain(graph, [(0 for _ in range(33))]), 2):
-		print('▒▒', end='')
-		for px1, px2 in zip(row1, row2):
-			print(' ▀▄█'[~(px1 | (px2 << 1))], end='')
-		print('▒▒')
-	print('▒' * 37)
+	def put_position(self, i, j):
+		pattern = [
+			[1,1,1,1,1,1,1],
+			[1,0,0,0,0,0,1],
+			[1,0,1,1,1,0,1],
+			[1,0,1,1,1,0,1],
+			[1,0,1,1,1,0,1],
+			[1,0,0,0,0,0,1],
+			[1,1,1,1,1,1,1],
+		]
+		for r in range(7):
+			for c in range(7):
+				self.pxl_on(i + r, j + c, pattern[r][c])
 
+	def put_alignment(self, i, j):
+		pattern = [
+			[1,1,1,1,1],
+			[1,0,0,0,1],
+			[1,0,1,0,1],
+			[1,0,0,0,1],
+			[1,1,1,1,1],
+		]
+		for r in range(5):
+			for c in range(5):
+				self.pxl_on(i + r, j + c, pattern[r][c])
 
-def put_position(i, j):
-	pattern = [
-		[1,1,1,1,1,1,1],
-		[1,0,0,0,0,0,1],
-		[1,0,1,1,1,0,1],
-		[1,0,1,1,1,0,1],
-		[1,0,1,1,1,0,1],
-		[1,0,0,0,0,0,1],
-		[1,1,1,1,1,1,1],
-	]
-	for r in range(7):
-		for c in range(7):
-			pxl_on(i + r, j + c, pattern[r][c])
+	def put_timing(self):
+		for j in range(7, self.size - 7):
+			self.pxl_on(6, j, 1 - j % 2)
+		for i in range(7, self.size - 7):
+			self.pxl_on(i, 6, 1 - i % 2)
+		self.pxl_on(self.size - 8, 8)
 
+	def put_format(self, bits=FORMAT_L0):
+		# bits = 0b111111111111111
+		# 0b111011111000100
+		for a in range(6):
+			self.pxl_on(a, 8, (FORMAT_L0 >> a) & 1)
 
-def put_alignment(i, j):
-	pattern = [
-		[1,1,1,1,1],
-		[1,0,0,0,1],
-		[1,0,1,0,1],
-		[1,0,0,0,1],
-		[1,1,1,1,1],
-	]
-	for r in range(5):
-		for c in range(5):
-			pxl_on(i + r, j + c, pattern[r][c])
+		self.pxl_on(7, 8, (FORMAT_L0 >> 6) & 1)
+		self.pxl_on(8, 8, (FORMAT_L0 >> 7) & 1)
+		self.pxl_on(8, 7, (FORMAT_L0 >> 8) & 1)
 
+		for a in range(6):
+			self.pxl_on(8, 5 - a, (FORMAT_L0 >> 9 + a) & 1)
 
-def put_timing(size=33):
-	for j in range(7, size - 7):
-		pxl_on(6, j, 1 - j % 2)
-	for i in range(7, size - 7):
-		pxl_on(i, 6, 1 - i % 2)
-	pxl_on(size - 8, 8)
+		for a in range(8):
+			self.pxl_on(8, self.size- 1 - a, (FORMAT_L0 >> a) & 1)
 
+		for a in range(7):
+			self.pxl_on(self.size - 7 + a, 8, (FORMAT_L0 >> 8 + a) & 1)
+	
+	def setup(self):
+		self.put_position(0, 0)
+		self.put_position(0, self.size - 7)
+		self.put_position(self.size - 7, 0)
+		self.put_alignment(self.size - 9, self.size - 9)
+		self.put_timing()
+		self.put_format()
 
-def put_format(bits=FORMAT_L0, size=33):
-	# bits = 0b111111111111111
-	# 0b111011111000100
-	for a in range(6):
-		pxl_on(a, 8, (FORMAT_L0 >> a) & 1)
+	def top_row(self, j):
+		return 9 if j < 9 or j > self.size - 9 else 0
 
-	pxl_on(7, 8, (FORMAT_L0 >> 6) & 1)
-	pxl_on(8, 8, (FORMAT_L0 >> 7) & 1)
-	pxl_on(8, 7, (FORMAT_L0 >> 8) & 1)
+	def bottom_row(self, j):
+		return self.size - 9 if j < 9 else self.size - 1
 
-	for a in range(6):
-		pxl_on(8, 5 - a, (FORMAT_L0 >> 9 + a) & 1)
+	def skip(self, i, j):
+		return i == 6 or abs(i - self.size + 7) <= 2 and abs(j - self.size + 7) <= 2
 
-	for a in range(8):
-		pxl_on(8, size - 1 - a, (FORMAT_L0 >> a) & 1)
+	def put_codewords(self, codewords):
+		i = self.size - 1
+		col = self.size - 1
+		d = -1
+		end = 9
+		index = 0
+		curr = codewords[0]
+		power = 128
+		
+		while col > 0:
+			for j in (col, col - 1):
+				if not self.skip(i, j):
+					
+					# no-bitwise mode
+					b = int(curr >= power)
+					curr -= b * power
+					power //= 2
+					if power < 1:
+						power = 128
+						index += 1
+						curr = codewords[index]
+						# Always add remainder just so we don't go out of bounds
 
-	for a in range(7):
-		pxl_on(size - 7 + a, 8, (FORMAT_L0 >> 8 + a) & 1)
+					self.pxl_on(i, j, b ^ mask0(i, j))
+					
+			if i == end:
+				d = -d
+				col -= (2 + (col == 8))
+				i = self.top_row(col)
+				end = self.bottom_row(col)
+				if d == -1:
+					i, end = end, i
+			else:
+				i += d
 
+	
+	def __eq__(self, other):
+		return isinstance(other, Matrix) and self.matrix == other.matrix
+	
+	def __xor__(self, other):
+		if not isinstance(other, Matrix) or self.size != other.size:
+			raise ValueError(other)
+			
+		x = Matrix(self.size)
+		for i, (row1, row2) in enumerate(zip(self.matrix, other.matrix)):
+			for j, (col1, col2) in enumerate(zip(row1, row2)):
+				x.pxl_on(i, j, col1 ^ col2)
+		return x
+	
+	def __repr__(self):
+		return f"[Matrix: {self.size}x{self.size}]"
 
 
 def mask0(i, j):
 	return (i + j) % 2 == 0
-
-
-# for i in range(33):
-	# for j in range(33):
-		# pxl_on(i, j, abs(i - 26) <= 2 and abs(j - 26) <= 2 or abs(i - 8) <= 2 and abs(j - 26) <= 2 or abs(i - 26) <= 2 and abs(j - 8) <= 2)
-
-
-# disp_graph()
-# sys.exit()
-
-put_position(0, 0)
-put_position(0, 26)
-put_position(26, 0)
-put_alignment(24, 24)
-put_timing()
-put_format()
-
-
-def skip(i, j):
-	return i == 6 or abs(i - 26) <= 2 and abs(j - 26) <= 2
 
 
 def iter_int_bits(n):
@@ -141,28 +187,30 @@ def iter_int_bits(n):
 		yield (n >> i) & 1
 
 
-def get_ecc_bytes(data, n_ecc=20):
+def get_ecc_bytes(data, rs_poly):
+	n_ecc = len(rs_poly)
 	ecc = [0] * n_ecc
 	for r, b in enumerate(data):
 		offset = r % n_ecc
 		factor = b ^ ecc[offset]
 		ecc[offset] = 0
 		if factor != 0:
-			for i, coef in enumerate(RS_POLY_20_LOG):
-				ecc[(i + r + 1) % n_ecc] ^= GF256_EXP[(coef + GF256_LOG[factor]) % 255]
+			for i, coef in enumerate(rs_poly):
+				ecc[(i + r + 1) % n_ecc] ^= GF256_EXP[(GF256_LOG[coef] + GF256_LOG[factor]) % 255]
 
 	return bytes(ecc)
 
 
-def get_ecc_bytes0(data, n_ecc=20):
+def get_ecc_bytes(data, rs_poly, blocks=1):
+	n_ecc = len(rs_poly) // blocks
 	ecc = deque(0 for _ in range(n_ecc))
 	for b in data:
 		factor = b ^ ecc[0]
 		ecc[0] = 0
 		ecc.rotate(-1)
 		if factor != 0:
-			for i, coef in enumerate(RS_POLY_20_LOG):
-				ecc[i] ^= GF256_EXP[(coef + GF256_LOG[factor]) % 255]
+			for i, coef in enumerate(rs_poly):
+				ecc[i % n_ecc] ^= GF256_EXP[(GF256_LOG[coef] + GF256_LOG[factor]) % 255]
 
 	return bytes(ecc)
 
@@ -241,86 +289,58 @@ class BitBuffer:
 		self.put(0, 4)
 
 
-def get_codewords(message, mode=BitBuffer.binary):
+
+DATA_LENGTH = [None, 19, 34, 55, 80, 108, 136, 156, 194, 232, 274, 324]
+
+
+def get_codewords(message, version, encoding):
 	buffer = BitBuffer()
-	mode(buffer, message)
+	if not callable(encoding):
+		encoding = {
+			'b': BitBuffer.binary,
+			'a': BitBuffer.alphanumeric,
+		}[encoding]
+	encoding(buffer, message)
 	codewords = buffer.buffer
 	
-	for f in range(0, 80 - len(codewords)):
+	print(' '.join(f"{b:02X}" for b in codewords))
+	
+	data_length = DATA_LENGTH[version]
+	for f in range(0, data_length - len(codewords)):
 		codewords.append([0xEC, 0x11][f % 2])
-
-	codewords.extend(get_ecc_bytes(codewords))
+	
+	if version < 6:
+		codewords.extend(get_ecc_bytes(codewords, RS_POLY[version]))
+	elif version < 11:
+		block1 = codewords[:data_length // 2]
+		block2 = codewords[data_length // 2:]
+		rs_poly = RS_POLY[version]
+		ecc1 = get_ecc_bytes(block1, rs_poly, 2)
+		ecc2 = get_ecc_bytes(block2, rs_poly, 2)
+		codewords = block1 + block2 + ecc1 + ecc2
+	else:
+		raise ValueError()
+	
 	codewords.append(0)
 	return codewords
 
 
-def top_row(j):
-	return 9 if j < 9 or j > 24 else 0
+def generate(message, version, encoding):
+	size = 21 + 4 * (version - 1)
+	matrix = Matrix(size)
+	matrix.setup()
+	correct = verify(message, version, encoding)
 
-
-def bottom_row(j):
-	return 24 if j < 9 else 32
-	
-	
-def _ti_bottom_row(J):
-	return 32-8*(J<9)
-def _ti_top_row(J):
-	return 9*(J<9 or J>24)
-
-
-def put_codewords(message, size=33):
-	i = size - 1
-	col = size - 1
-	d = -1
-	end = 9
-	codewords = get_codewords(message, BitBuffer.alphanumeric)
-	
-	print(' '.join(f"{b:02X}" for b in codewords))
-	
-	index = 0
-	curr = codewords[0]
-	power = 128
-	
-	while col > 0:
-		for j in (col, col - 1):
-			if not skip(i, j):
-				
-				# no-bitwise mode
-				b = int(curr >= power)
-				curr -= b * power
-				power //= 2
-				if power < 1:
-					power = 128
-					index += 1
-					curr = codewords[index]
-					# Always add remainder just so we don't go out of bounds
-				
-				pxl_on(i, j, b ^ mask0(i, j))
-
-		if i == end:
-			d = -d
-			col -= (2 + (col == 8))
-			i = top_row(col)
-			end = bottom_row(col)
-			if d == -1:
-				i, end = end, i
+	try:
+		codewords = get_codewords(message, version, encoding)
+		matrix.put_codewords(codewords)
+	finally:
+		if matrix == correct:
+			matrix.disp()
+			print('Correct!')
 		else:
-			i += d
+			(matrix ^ correct).disp()
+			print('Incorrect! Diff displayed.')
 
 
-try:
-	
-	message = ' '.join(sys.argv[1:])
-	put_codewords(message)
-	correct = verify_nayuki_alphanumeric(message)
-
-finally:
-	if matrix == correct:
-		disp_graph()
-		print('Correct!')
-	else:
-		diff = [
-			[col1 ^ col2 for col1, col2 in zip(row1, row2)] 
-		for row1, row2 in zip(matrix, correct)]
-		disp_graph(diff)
-		print('Incorrect! Diff displayed.')
+generate(' '.join(sys.argv[1:]), 5, 'b')
