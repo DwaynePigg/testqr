a77dcb95c55629eca9cdf8b0013cafb858be43cc
Max <maxkoren@gmail.com>
2026-03-31 01:18:49 -0400

    final candidate

--- ti.py
+++ ti.py
@@ -387,7 +387,7 @@ if V>1:
 		L1 = L_[6,M,M,M,S-7,S-7]
 		L2 = L_[M,6,M,S-7,M,S-7]
 		T = 2*V+2
-		Y1 = lambda: I==6 or abs(T*fPart(I/T)-6)<=2.1 and abs(T*fPart(J/T)-6)<=2.1 and (abs(I-J)<=S/2-2) or J>=S-11 and I<=5
+		Y1 = lambda: I==6 or abs(T*fPart(I/T)-6)<=2.1 and abs(T*fPart(J/T)-6)<=2.1 and abs(I-J)<=S/2-2 or J>=S-11 and I<=5
 		Y1 = lambda: I==6 or max((abs(I-L1)<=2) & (abs(J-L2)<=2)) or J>=S-11 and I<=5
 
 	for X in For(1,dim(L1)):
